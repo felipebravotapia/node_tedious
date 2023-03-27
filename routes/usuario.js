65bfbd575392ed6137518c1d68ee65568a8befcb
Felipe <fbravo@pentafinanciero.cl>
2023-03-27 08:56:41 -0300

    mod

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 const DataController = require("../controller/dataController");
 
 router.post("/", async (req, res, next) => {
   try {
-    const data = await DataController.insertData(
-      req.body.username,
-      req.body.clave
-    );
-    res.json(data);
+
+    
+    let encriptado = await bcrypt
+      .genSalt(10)
+      .then((salt) => {
+        console.log("Salt:", 10);
+        return bcrypt.hash(req.body.clave, 10);
+      })
+      .then((hash) => {
+        return hash;
+      })
+      .catch((err) => console.error(err.message));
+
+    const data = await DataController.insertData(req.body.username, encriptado);
+    // res.json(data);
+    res.json("ok");
   } catch (err) {
     console.error("Error al Insertar:", err);
     res.status(500).send("Error al obtener datos");
